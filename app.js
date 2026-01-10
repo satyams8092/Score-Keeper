1c1b775f5bf6904418115421ea21dc5396d62961
SATYAM SHAH <satyam.s8092@gmail.com>
2026-01-11 00:03:21 +0530

    logic done

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const p1Button=document.querySelector('#p1Button')
 const p2Button=document.querySelector('#p2Button')
+const reset=document.querySelector('#reset')
 const p1Display=document.querySelector('#p1Display')
 const p2Display=document.querySelector('#p2Display')
+const winScoreSelect=document.querySelector('#playTo')
 
 let p1Score=0
 let p2Score=0
-let winScore=5
+let winScore=3
 let isGameOver=false
 
 p1Button.addEventListener('click',function(){
@@ -13,16 +15,38 @@ p1Button.addEventListener('click',function(){
         p1Score+=1
         if(p1Score === winScore){
             isGameOver=true
+            p1Display.classList.add('winner')
+            p2Display.classList.add('losser')
         }
         p1Display.textContent=p1Score
     }
 })
+
 p2Button.addEventListener('click',function(){
     if(!isGameOver){
         p2Score+=1
         if(p2Score === winScore){
             isGameOver=true
+            p2Display.classList.add('winner')
+            p1Display.classList.add('losser')
         }
         p2Display.textContent=p2Score
     }
 })
+
+winScoreSelect.addEventListener('change',function(){
+    winScore=parseInt(this.value)
+    resetGame()
+})
+
+reset.addEventListener('click',resetGame)
+
+function resetGame() {
+    isGameOver=false
+    p1Score=0
+    p2Score=0
+    p1Display.textContent=p1Score
+    p2Display.textContent=p2Score
+    p1Display.classList.remove('winner','losser')
+    p2Display.classList.remove('winner','losser')
+}
